a557034792730b1df1107f69ba93c5e3abbe7fe4
John Else <john.else@citrix.com>
2014-01-29 13:59:46 +0000

    Improve docs
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/crc.mli
+++ lib/crc.mli
@@ -14,16 +14,17 @@
  * PERFORMANCE OF THIS SOFTWARE.
  *)
 
+(** CRC library for strings and cstructs. *)
+
 exception Invalid_length
 exception Invalid_offset
 
+(** This is the algorithm used by HDLC, ANSI X3.66, ITU-T V.42,
+	Ethernet, Serial ATA, MPEG-2, PKZIP, Gzip, Bzip2, PNG and
+	others:
+	http://en.wikipedia.org/wiki/Cyclic_redundancy_check#Commonly_used_and_standardized_CRCs
+*)
 module Crc32 : sig
-	(** This is the algorithm used by HDLC, ANSI X3.66, ITU-T V.42,
-	  Ethernet, Serial ATA, MPEG-2, PKZIP, Gzip, Bzip2, PNG and
-	  others:
-	  http://en.wikipedia.org/wiki/Cyclic_redundancy_check#Commonly_used_and_standardized_CRCs
-	*)
-
 	val cstruct: ?crc:int32 -> Cstruct.t -> int32
 	(** [cstruct ?crc buf] computes the CRC of [buf] with optional
 	    initial value [crc] *)
